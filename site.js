5b5a9207e01e8226d51c9d64d8a709b6af18d3ec
Samuele Marcuccci <marcucci.sam@gmail.com>
2026-04-21 19:03:32 -0400

    Update site

--- site.js
+++ site.js
@@ -1,1 +1,7 @@
+const syncHeaderState = () => {
+  document.body.classList.toggle("scrolled", window.scrollY > 8);
+};
 
+window.addEventListener("scroll", syncHeaderState, { passive: true });
+window.addEventListener("load", syncHeaderState);
+syncHeaderState();
